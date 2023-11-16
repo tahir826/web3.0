2a40cbd6d9c581e456015c017a2dadcfbf6f8e33
tahir826 <151031485+tahir826@users.noreply.github.com>
2023-11-16 17:16:43 +0500

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////// Task # 1 //////////////////////////////////////////////////////////////////////
+// Declare variables for a person's name, age, and whether they are a student, Print these variables to the console.
+var username = "Tahir";
+var age = "18";
+var student = "true";
+console.log (username);
+console.log (age);
+console.log (student);
+///////////////////////////////////////////////// Task # 2 /////////////////////////////////////////////////////////////////////
+// Declare two variables with numeric values, Perform addition, subtraction, multiplication, and division on these variables, Print the results to the console.
+var num1 = 11
+var num2 = 22
+var num3 = 33
+var num4 = 44
+var num5 = 55
+var num6 = 66
+var num7 = 77
+var num8 = 88
+var num9 = 99
+var num10 = 110
+console.log (num1+num2+num3+num4+num5+num6+num7+num8+num9+num10);
+console.log (num1-num2-num3-num4-num5-num6-num7-num8-num9-num10);
+console.log (num1*num2*num3*num4*num5*num6*num7*num8*num9*num10);
+console.log (num1/num2/num3/num4/num5/num6/num7/num8/num9/num10);
+/////////////////////////////////////////////// Task # 3 ///////////////////////////////////////////////////////////////////////////
+// // Declare a string variable with your favorite programming language, Print the final sentence to the console.
+let favorite_language: string = "Typesript";
+console.log (`My favorite language is = ${favorite_language}.`)
